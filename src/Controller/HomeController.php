633ad3e99953ff696ff6caeb44fafa9d12e29292
Hamza Benketaf <hamza.benketaf@gmail.com>
2022-08-22 16:59:41 +0200

    home & display rooms

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Chambre;
+use App\Repository\ChambreRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class HomeController extends AbstractController
 {
-  #[Route('/', name: 'app_home')]
-  public function index(): Response
+  #[Route('/', name: 'app_home', methods: ['GET', 'POST'])]
+  public function index(ChambreRepository $chambreRepository): Response
   {
+    $chambres = $chambreRepository->findAll();
     return $this->render('home/index.html.twig', [
-      'controller_name' => 'HomeController',
+      'chambres' => $chambres
+    ]);
+  }
+
+  #[Route('/chambre/{id}', name: 'chambre', methods: ['GET', 'POST'])]
+  public function chambre(Chambre $chambre): Response
+  {
+    return $this->render('home/chambre.html.twig', [
+      'chambre' => $chambre
     ]);
   }
 }
